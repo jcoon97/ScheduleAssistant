1e9ed71f822444ac0ba0d75300745d3ef753e21f
James Coon <44541968+jcoon97@users.noreply.github.com>
2021-11-09 19:01:05 -0500

    Remove inheritance from AssignUserProgramInput

--- packages/server/src/input-types/ChangeProgramLeadOrManagerInput.ts
+++ packages/server/src/input-types/ChangeProgramLeadOrManagerInput.ts
@@ -2,12 +2,9 @@ import { Field, ID, InputType } from "type-graphql";
 import { Program } from "../entities/Program";
 import { User } from "../entities/User";
 import { IsEntityFound } from "../validators/entity";
-import { AssignUserProgramInput } from "./AssignUserProgramInput";
-
-type IChangeProgramLeadOrManagerInput = Required<Pick<AssignUserProgramInput, "programId" | "userId">>;
 
 @InputType("ChangeProgramLeadOrManagerInput")
-export class ChangeProgramLeadOrManagerInput implements IChangeProgramLeadOrManagerInput {
+export class ChangeProgramLeadOrManagerInput {
     @Field(() => ID)
     @IsEntityFound(Program, "id", {
         message: "Program could not be found by specified ID"
